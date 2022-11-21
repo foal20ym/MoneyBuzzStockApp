567ee01e332b2d5ab77c224cd5da49113d4119e6
foal20ym <alexander-forsanker2011@hotmail.com>
2022-11-21 20:46:24 +0100

    Created the StockData Model.

---
+++ MoneyBuzz/StockData.swift
@@ -0,0 +1,52 @@
+//
+//  StockData.swift
+//  MoneyBuzz
+//
+//  Created by Alexander Forsanker on 11/21/22.
+//
+
+import Foundation
+
+struct StockData: Codable {
+    struct MetaData: Codable {
+        let information: String
+        let symbol: String
+        let lastRefreshed: String
+        let interval: String
+        let outputSize: String
+        let timeZone: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case information = "1. Information"
+            case symbol = "2. Symbol"
+            case lastRefreshed = "3. Last Refreshed"
+            case interval = "4. Interval"
+            case outputSize = "5. Output Size"
+            case timeZone = "6. Time Zone"
+        }
+    }
+    
+    struct StockDataEntry: Codable {
+        let open: String
+        let high: String
+        let low: String
+        let close: String
+        let volume: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case open = "1. open"
+            case high  = "2. high"
+            case low  = "3. low"
+            case close  = "4. close"
+            case volume  = "5. volume"
+        }
+    }
+    
+    let metaData: MetaData
+    let timeSeries5min: [String: StockDataEntry]
+    
+    private enum CodingKeys: String, CodingKey {
+        case metaData = "Meta Data"
+        case timeSeries5min = "Time Series (5min)"
+    }
+}
